6c232733e5c51807f0bcc77bf87ee8574cc25ff5
rudevdr <gudayschief@gmail.com>
2018-11-30 04:18:45 +0530

    Contains all the infos about locations

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+export const Data =  [
+	{id: 0, lat: 25.422036, lng: 81.881275, infoText: ' Considered one of the holiest places in India for followers of Hinduism, Triveni Sangam in Allahabad is the confluence of three rivers, Yamuna, Ganga and the mythical Saraswati.', name: "Triveni Sangam", imgUrl: "http://prtsc.ca/images/2018/11/29/2AxIY4M.jpg" },
+
+	{id: 1, lat: 25.4294, lng: 81.876855, infoText: 'Allahabad Fort is a fort built by the Mughal emperor Akbar at Prayagraj, Uttar Pradesh, India in 1583.', name: "Allahabad Fort", imgUrl: "http://prtsc.ca/images/2018/11/29/2BFty03.jpg"},
+
+	{id: 2, lat: 25.442271, lng: 81.821039, infoText: 'Khusrau Bagh is a large walled garden and burial complex located in muhalla Khuldabad close to the Allahabad Junction Station, in Allahabad.', name: "Khusro Bagh", imgUrl: "http://prtsc.ca/images/2018/11/29/2RnTtPl.jpg" },
+
+	{id: 3, lat: 25.459376, lng: 81.859982, infoText: 'The Anand Bhavan is a historic house museum in Allahabad, India focusing on the Nehru Family.', name: "Anand Bhavan", imgUrl: "http://prtsc.ca/images/2018/11/29/Anand_Bhawan2C_Allahabad.jpg" },
+
+	{id: 4, lat: 25.45121, lng: 81.826639, infoText: 'Modeled after 13th-century Gothic style churches, it is among the Gothic Revival buildings built by the British during their rule in India.', name: "All Saint Cathedral Church", imgUrl: "http://prtsc.ca/images/2018/11/29/220px-All_Saints27_Cathedral.jpg" },
+
+	{id: 5, lat: 25.466936, lng: 81.859411, infoText: 'Established on 23 September 1887, it is the fourth oldest modern university in India.', name: "Allahabad University", imgUrl: "http://prtsc.ca/images/2018/11/29/poster.jpg"},
+
+	{id: 6, lat: 25.416225, lng: 81.848211, infoText: "The Old Naini Bridge is one of the longest and oldest bridges in India, located in Prayagraj. It is a double-decked steel truss bridge which runs across the Yamuna river.", name: "Old Naini Bridge", imgUrl: "http://prtsc.ca/images/2018/11/29/Allahabad_railwaybridge1870.jpg"},
+
+	{id: 7, lat: 25.457132, lng: 81.838301, infoText: "Chandrasekhar Azad Park is a public park in Prayagraj. Built in 1870 to mark Prince Alfred's visit to the city, with an area of 133 acres, it is the biggest park in Allahabad. It was renamed after freedom fighter Chandra Shekhar Azad, who sacrificed his life here, during the Indian independence struggle in 1931.", name: "Chandrasekhar Azad Park", imgUrl: "http://prtsc.ca/images/2018/11/29/59121275.jpg"}
+]
